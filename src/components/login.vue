ef54ea95d0a9ca0dee57a47d973d4a7741c46716
陈童 <1421911991@qq.com>
2021-01-27 19:20:22 +0800

    实现表单数据校验

--- src/components/login.vue
+++ src/components/login.vue
@@ -6,13 +6,13 @@
         <img src="../assets/logo.png" alt="" />
       </div>
       <!-- 表单区域 -->
-      <el-form class="login_form" :model="loginForm">
+      <el-form class="login_form" :model="loginForm" :rules="loginFormRules">
         <!-- 用户名 -->
-        <el-form-item>
+        <el-form-item prop="username">
           <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
         </el-form-item>
         <el-form-item class="btns">
@@ -31,6 +31,35 @@ export default {
       loginForm: {
         username: 'admin',
         password: '123456'
+      },
+      loginFormRules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          },
+          {
+            min: 3,
+            max: 10,
+            message: '长度在 3 到 10 个字符之间',
+            trigger: 'blur'
+          }
+        ],
+        // 验证密码
+        password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 15,
+            message: '长度在 6 到 15 个字符之间',
+            trigger: 'blur'
+          }
+        ]
       }
     }
   }
