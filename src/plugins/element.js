969ff40881c31f2611f616195f4578da2db9a782
陈童 <1421911991@qq.com>
2021-01-29 18:00:19 +0800

    绘制用户列表组件的基本结构

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Header, Container, Aside, Main, Menu, Submenu, MenuItem } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Header, Container, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -12,6 +12,11 @@ Vue.use(Main)
 Vue.use(Menu)
 Vue.use(Submenu)
 Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
 
 // 把弹框组件挂着到了 vue 的原型对象上，这样每一个组件都可以直接通过 this 访问
 Vue.prototype.$message = Message
