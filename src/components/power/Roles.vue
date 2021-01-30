2305bb06975fa9a0b9fcfd4188e8794447c1933c
陈童 <1421911991@qq.com>
2021-01-30 19:28:53 +0800

    获取角色列表数据

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    角色列表
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      123
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      rolesList: []
+    }
+  },
+  created() {
+    this.getRolesList()
+  },
+  methods: {
+    async getRolesList() {
+      const { data: res } = await this.$http.get('roles')
+      if (res.meta.status !== 200) return this.$message.error('获取角色信息失败')
+      this.rolesList = res.data
+      console.log(this.rolesList)
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped></style>
