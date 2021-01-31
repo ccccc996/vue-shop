4155fd343ee036fbcbfebed34a8deafd61bb9cad
陈童 <1421911991@qq.com>
2021-01-31 15:38:37 +0800

    绘制商品分类基本结构

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -1,9 +1,32 @@
 <template>
-  <div>商品</div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格区域 -->
+      <!-- 分页区域 -->
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
 </script>
 
 <style lang="less" scoped></style>
