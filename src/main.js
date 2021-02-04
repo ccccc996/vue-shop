817bd52a5b5810353135b7d38503127f08e60b4f
陈童 <1421911991@qq.com>
2021-02-04 19:02:36 +0800

    预校验，处理goods_cat和attrs并添加商品

--- src/main.js
+++ src/main.js
@@ -2,13 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
 
 import './assets/fonts/iconfont.css'
 // 导入全局样式
 import './assets/css/global.css'
+// 导入富文本编辑器样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 
 import TreeTable from 'vue-table-with-tree-grid'
 
+Vue.use(VueQuillEditor)
 //引入axios
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
