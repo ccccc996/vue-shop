ffcdd776d630fa3083a77054677ed472b932cbc2
陈童 <1421911991@qq.com>
2021-02-05 14:35:47 +0800

    渲染订单列表分页

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -8,15 +8,36 @@
     </el-breadcrumb>
     <!-- 卡片视图区域 -->
     <el-card>
-      <el-card>
-        <el-row>
-          <el-col :span="8">
-            <el-input placeholder="请输入内容">
-              <el-button slot="append" icon="el-icon-search"></el-button>
-            </el-input>
-          </el-col>
-        </el-row>
-      </el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+      <!-- 订单列表数据 -->
+      <el-table :data="orderlist" border stripe>
+        <el-table-column prop="order_number" label="订单编号"> </el-table-column>
+        <el-table-column prop="order_price" label="订单价格"> </el-table-column>
+        <el-table-column prop="order_status" label="是否付款">
+          <template>
+            <el-tag type="success">已付款</el-tag>
+            <el-tag type="danger">未付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货" prop="is_send"></el-table-column>
+        <el-table-column label="下单时间" prop="create_time">
+          <template slot-scope="scope">{{ scope.row.create_time | dateFormat }}</template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button size="mini" type="primary" icon="el-icon-edit"></el-button>
+            <el-button size="mini" type="success" icon="el-icon-location"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页功能 -->
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="queryInfo.pagenum" :page-sizes="[5, 10, 15]" :page-size="queryInfo.pagesize" layout="total, sizes, prev, pager, next, jumper" :total="total"> </el-pagination>
     </el-card>
   </div>
 </template>
@@ -24,7 +45,37 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      orderlist: [],
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 5
+      },
+      total: 0
+    }
+  },
+  created() {
+    this.getOrderList()
+  },
+  methods: {
+    async getOrderList() {
+      const { data: res } = await this.$http.get('orders', {
+        params: this.queryInfo
+      })
+    //   console.log(res)
+      if (res.meta.status !== 200) return this.$message.error('获取订单列表失败')
+      this.total = res.data.total
+      this.orderlist = res.data.goods
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getOrderList()
+    }
   }
 }
 </script>
